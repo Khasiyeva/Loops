539692eb053b3bcccf9adcacbd6734c03e9b453e
Khasiyeva <bd2q9mvwb@code.edu.az>
2023-09-28 00:47:51 +0400

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿//Task1
+//int n = 32;
+//if (n == 1)
+//{
+//    Console.WriteLine("Yes");
+//}
+//for(int i = n; i > 1; i /= 2)
+//{
+//    if (i / 2 == 1)
+//    {
+//        Console.WriteLine("Yes");
+//        break;
+//    }
+//    if (i % 2 == 1)
+//    {
+//        Console.WriteLine("No");
+//        break;
+//    }
+//}
+
+
+
+//Task2
+int num = 11;
+if (num == 0 || num == 1)
+{
+    Console.WriteLine(" It is not prime or complex number ");
+}
+else
+{
+    for (int i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            Console.WriteLine("It is not prime number");
+            return;
+        }
+
+    }
+    Console.WriteLine(" It is a prime number");
+}
